e46a50408d467c7d83fd2857f69da1fe1d21546d
Tan Jun Xiang <tjunxiang92@gmail.com>
2016-05-31 19:48:08 +0800

    Framework of the conversion

---
+++ process.py
@@ -0,0 +1,11 @@
+import xmltodict
+
+test = {a: 'hello', b: ['h1', 'h2']}
+
+## Generate a table
+def process_dict(dd):
+	pass
+
+## Generate a table with reference to the foreign key
+def process_list(ll, main_table, main_key):
+	pass
